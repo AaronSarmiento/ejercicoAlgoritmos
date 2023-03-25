6a61557a4867714de3533dab716a281f35b02eff
Sala Golden <golden@academia.loc>
2023-03-24 21:49:37 -0500

    Primer Commit

---
+++ src/launcher/Pruebas.java
@@ -0,0 +1,18 @@
+package launcher;
+
+import ordenamiento.AaronSarmiento;
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AaronSarmiento as = null;
+		String[] num= {"56", "12", "64", "34", "87", "56"};
+		
+		System.out.println(as.ordenarAsc(num));
+		System.out.println(as.ordenarDesc(num));
+
+	}
+
+}
---
+++ src/ordenamiento/AaronSarmiento.java
@@ -0,0 +1,9 @@
+package ordenamiento;
+
+public interface AaronSarmiento {
+	
+	public String ordenarAsc(String[] numbers); 
+	public String ordenarDesc(String[] numbers);	
+	
+
+}
